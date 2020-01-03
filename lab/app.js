98624cd314addf9c8bf13603920d2f07994294c3
Sian Culligan <SianCulligan@gmail.com>
2020-01-03 12:47:55 -0800

    Completion of lab B

--- lab/app.js
+++ lab/app.js
@@ -7,18 +7,18 @@ const theOldWay = function(course) {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('The old way:', theOldWay('Code 301'));
+console.log('The old way:', theOldWay('Code 301'));
 
 
 // STEP 2
 // We can refactor our first function to use an arrow function.
-// The word "function" is removed and an arrow is added in between the parameter and the opening curly brace
+// // The word "function" is removed and an arrow is added in between the parameter and the opening curly brace
 const theNewWay = (course) => {
   return `I am currently enrolled in ${course}`;
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('The new way:', theNewWay('Code 301'));
+console.log('The new way:', theNewWay('Code 301'));
 
 
 // STEP 3
@@ -28,7 +28,7 @@ const withoutParens = course => {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('Without parens:', withoutParens('Code 301'));
+console.log('Without parens:', withoutParens('Code 301'));
 
 
 // STEP 4
@@ -38,7 +38,7 @@ const withoutParens = course => {
 const oneLiner = course => `I cam currently enrolled in ${course}`;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('As a one-liner:', oneLiner('Code 301'));
+console.log('As a one-liner:', oneLiner('Code 301'));
 
 
 // STEP 5
@@ -49,7 +49,7 @@ const add = function(num1, num2) {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('Let\'s do some math:', add(4, 5));
+console.log('Let\'s do some math:', add(4, 5));
 
 
 // STEP 6
@@ -58,7 +58,7 @@ const add = function(num1, num2) {
 const addOneLiner = (num1, num2) => `${num1} + ${num2} = ${num1 + num2}`;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('Add as a one-liner:', addOneLiner(4, 5));
+console.log('Add as a one-liner:', addOneLiner(4, 5));
 
 
 // STEP 7
@@ -70,7 +70,7 @@ const multiLiner = word => {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('Multi-line arrow function:', multiLiner('hello'));
+console.log('Multi-line arrow function:', multiLiner('hello'));
 
 
 // STEP 8
@@ -85,7 +85,7 @@ const oldObject = function(array) {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('Hello from the old object function', oldObject(['hi', 'hello', 'are you there?']));
+console.log('Hello from the old object function', oldObject(['hi', 'hello', 'are you there?']));
 
 
 // STEP 9
@@ -98,7 +98,7 @@ const newObject = array => ({
 });
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('Hello from the new object function', newObject(['hi', 'hello', 'are you there?']));
+console.log('Hello from the new object function', newObject(['hi', 'hello', 'are you there?']));
 
 
 // STEP 10
@@ -107,45 +107,67 @@ const newObject = array => ({
 // Write your solutions on a single line wherever possible.
 
 
+// ORIGINAL CODE
+// let sum = function(a, b, c, d) {
+//   return a + b + c + d;
+// };
 
-let sum = function(a, b, c, d) {
-  return a + b + c + d;
-};
+// REFACTORED CODE
+let sum = (a, b, c, d) => `${a + b + c + d}`;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sum(1, 2, 3, 4));
+console.log(sum(1, 2, 3, 4));
 
+// ORIGINAL CODE
+// let objectLit = function() {
+//   return {
+//     key1: 'value1',
+//     key2: 'value2',
+//     key3: 'value3',
+//   };
+// };
 
-let objectLit = function() {
-  return {
-    key1: 'value1',
-    key2: 'value2',
-    key3: 'value3',
-  };
-};
+// REFACTORED CODE
+const objectLit2 = () => ({
+  key1: 'value1',
+  key2: 'value2',
+  key3: 'value3',
+});
+
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // console.log(objectLit());
+console.log(objectLit2());
 
-
+// ORIGINAL CODE
 let sumAndProduct = function(a, b) {
   let sum = a + b;
   let product = a * b;
   return [sum, product];
 };
 
+// REFACTORED CODE
+let sumAndProduct2 = (a, b) => {return [a + b, a * b]};
+
+
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sumAndProduct(3, 9));
+console.log(sumAndProduct(3, 9));
+console.log(sumAndProduct2(3, 9));
 
-
+// ORIGINAL CODE
 let message = function(name) {
   return `Hello, ${name}!`;
 };
 
+// REFACTORED CODE
+let messageRefactored = name => `Hello, ${name}!`;
+
+
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(message('Allie'));
+console.log(message('Allie'));
+console.log(messageRefactored('Allie'));
 
-
+// ORIGINAL CODE
 let Student = function(name, age, hometown) {
   this.name = name;
   this.age = age;
@@ -154,11 +176,23 @@ let Student = function(name, age, hometown) {
 
 let joe = new Student('Joe', 'Schmoe', 100);
 
+
+// // REFACTORED CODE
+// let Student2 = (name, age, hometown) => {
+//   this.name = name;
+//   this.age = age;
+//   this.hometown = hometown;
+// };
+
+// let joe = new Student2('Joe', 'Schmoe', 100);
+
+
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this code to break!
-// console.log(joe);
+console.log(joe);
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
+// -----> Cannot use arrow function & 'this'  - 'this' bubbles up to the window object and no longer refers to the constructor function. To get the code working again, code just needs to be returned to the original code block.
 
 
 
@@ -168,10 +202,9 @@ Student.prototype.greeting = function() {
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
-// console.log(joe.greeting());
+console.log(joe.greeting());
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
-
 
 
 Student.courseName = function() {
@@ -179,28 +212,31 @@ Student.courseName = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+console.log(Student.courseName());
 
 
 
 // STEP 11
 // How do arrow functions affect constructor functions?
-Student.prototype.scope = function() {
-  console.log(this);
-};
+// Student.prototype.scope = function() {
+//   console.log(this);
+// };
 
-// TODO: Uncomment the following line of code to see the output in the browser console
+// // TODO: Uncomment the following line of code to see the output in the browser console
 // console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
-// TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+// // TODO: Uncomment the following line of code to see the output in the browser console
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
+// Student
 //
 // 2. What is "this" when joe.scopeArrow() is invoked?
+// The window object.
 //
 // 3. Explain why "this" is different when an arrow function is used.
+// -----> Cannot use arrow function & 'this'  - 'this' bubbles up to the window object and no longer refers to the constructor function. To get the code working again, code just needs to be returned to the original code block.
 //
